42c539568c9b77d2a774a9849795c931b092ca37
Mahan Hashemizadeh <mahanhz@gmail.com>
2017-08-13 23:08:35 +0200

    Open search results in an inline lightbox

---
+++ application/periphery/ui/src/main/resources/static/js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    $("#eventDetails").fancybox({
+        fitToView	: false,
+        width		: '70%',
+        height		: '70%',
+        autoSize	: false,
+        closeClick	: false,
+        openEffect	: 'none',
+        closeEffect	: 'none'
+    });
+});
