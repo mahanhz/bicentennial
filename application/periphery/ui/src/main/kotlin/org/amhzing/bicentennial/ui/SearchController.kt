0cee5366cf5f8c9855994b4ac2ad87c7c541ba95
Mahan Hashemizadeh <mahanhz@gmail.com>
2017-08-13 15:11:26 +0200

    Adding ability to get search criteria from UI

--- application/periphery/ui/src/main/kotlin/org/amhzing/bicentennial/ui/SearchController.kt
+++ application/periphery/ui/src/main/kotlin/org/amhzing/bicentennial/ui/SearchController.kt
@@ -2,14 +2,17 @@ package org.amhzing.bicentennial.ui
 
 import org.amhzing.bicentennial.adapter.SearchEventAdapter
 import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 
 @Controller
 class SearchController(private val searchEventAdapter: SearchEventAdapter) {
 
-    @GetMapping(path = arrayOf("/search"))
-    fun search() : String {
+    @PostMapping(path = arrayOf("/search"))
+    fun search(@RequestParam(value = "country") country: String,
+               @RequestParam(value = "address") address: String,
+               @RequestParam(value = "distance") distance: Int) : String {
         val events = searchEventAdapter.events(59.383526, 17.978573, 5)
 
         return "searchResults"
